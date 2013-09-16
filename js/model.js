6339f9e0d3182f2a0fa1fc7632cbc94d5aa5c33a
skyfox <skyfox@cubicle.fi>
2013-09-16 18:19:29 +0300

    ladataan produt vain yhden kerran

--- js/model.js
+++ js/model.js
@@ -3,21 +3,22 @@ app.factory('DemoModel', function (Product) {
     var factory = (function() {
         var instance = {};
         instance.products = [];
+        instance.ids = [];
         instance.refresh = function(ids) {
+            if(instance.products.length == 0) {
+                ids.forEach(function(id){
+                    Product.get({id: id}, function(result) {
+                        result.demo.download.url.forEach(function(url){
 
-            ids.forEach(function(id){
-                Product.get({id: id}, function(result) {
-                    result.demo.download.url.forEach(function(url){
-
-                        if(url['@attributes'].type === "youtube")
-                        {
-                           result.youtube = url.data.replace("http://www.youtube.com/watch?v=","//www.youtube.com/embed/");
-                        }
+                            if(url['@attributes'].type === "youtube")
+                            {
+                               result.youtube = url.data.replace("http://www.youtube.com/watch?v=","//www.youtube.com/embed/");
+                            }
+                        });
+                        instance.products.push(result);
                     });
-                    instance.products.push(result);
                 });
-            });
-
+            }
         }
 
         return instance;
