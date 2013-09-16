ddabdf832de46676f345a0e3f30dcf0fdd3a2433
skyfox <skyfox@cubicle.fi>
2013-09-16 08:09:16 +0300

    perustauhka

---
+++ product.php
@@ -0,0 +1,16 @@
+<?php
+
+header('Content-type: application/json');
+$id = intval(htmlspecialchars($_GET["id"]));
+
+if($id > 0) {
+
+	$xmlstring = file_get_contents("http://www.pouet.net/export/prod.xnfo.php?which=" . $id);
+	$xmlstring = eregi_replace("(<[a-z]+ [^>]*>)([^\n<]+)", "\\1<data>\\2</data>", $xmlstring);
+	$xml = simplexml_load_string($xmlstring);
+	echo json_encode($xml);
+
+}
+
+
+?>
