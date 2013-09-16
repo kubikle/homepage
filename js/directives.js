483e72b6ea10d091dbfcf9a8fa787dec852885f9
skyfox <skyfox@cubicle.fi>
2013-09-16 19:40:07 +0300

    Chrome fix

--- js/directives.js
+++ js/directives.js
@@ -4,7 +4,8 @@ app.directive('navActive', function($location, $animate) {
         link: function(scope, element, attr) {
 
             scope.$on("$routeChangeSuccess", function (event, current, previous) {
-                if($location.path().contains(attr.navActive)) {
+                console.log($location.path());
+                if($location.path().indexOf(attr.navActive) != -1) {
                     $animate.addClass(element, "active");
                 } else {
                     $animate.removeClass(element, "active");
