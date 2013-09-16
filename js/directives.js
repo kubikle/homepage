3a84e505fd3a0b7d94c89f9089d5491917b497b7
skyfox <skyfox@cubicle.fi>
2013-09-16 19:29:19 +0300

    Turhaa logitusta pois

--- js/directives.js
+++ js/directives.js
@@ -4,13 +4,9 @@ app.directive('navActive', function($location, $animate) {
         link: function(scope, element, attr) {
 
             scope.$on("$routeChangeSuccess", function (event, current, previous) {
-                console.log(attr.navActive);
-                console.log($location.path());
                 if($location.path().contains(attr.navActive)) {
-                    console.log("add");
                     $animate.addClass(element, "active");
                 } else {
-                    console.log("remove");
                     $animate.removeClass(element, "active");
                 }
             });
