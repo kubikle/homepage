d25b5d91bcd26840cba7442bccbfb57ad12fa1ad
skyfox <skyfox@cubicle.fi>
2013-10-21 11:26:11 +0300

    Update controller.js

--- js/controller.js
+++ js/controller.js
@@ -1,6 +1,6 @@
 function DemoController($scope, $location, $routeParams, $sce, DemoModel) {
     $scope.model = DemoModel;
-    DemoModel.refresh([59145,59170,57447,56995,56859,54609,18352,13041]);
+    DemoModel.refresh([62005,59145,59170,57447,56995,56859,54609,18352,13041]);
 
     $scope.trustSrc = function(src) {
         return $sce.trustAsResourceUrl(src);
